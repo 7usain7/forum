92bddc4c5269b5f56ee4661d60f87c64b604fc77
alimadan <ali-madan@outlook.sa>
2025-10-21 23:23:30 +0300

    Broken last commit

--- handler/handler.go
+++ handler/handler.go
@@ -76,7 +76,6 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 
 	errorType := r.URL.Query().Get("error")
 	errorType = errorString(errorType)
-	Data := prepareIndexData(posts, categories, filter, errorType)
 
 	// Get validation errors from temporary storage
 	var validationErrorList []string
@@ -108,26 +107,7 @@ func handleCreatePost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-<<<<<<< HEAD
-	title := r.FormValue("title")
-	if len(title) > 128 {
-		http.Redirect(w, r, "/?error=title_too_long#new-post", http.StatusSeeOther)
-		return
-	}
-	body := r.FormValue("body")
-	if len(body) > 512 {
-		http.Redirect(w, r, "/?error=body_too_long#new-post", http.StatusSeeOther)
-		return
-	}
-	if title == "" || body == "" {
-		http.Redirect(w, r, "/?error=empty", http.StatusSeeOther)
-		return
-	}
-
-	// Parse form to get categories
-=======
 	// Parse form to get all form data
->>>>>>> dedef62 (Make a backend validation to max title lenght and body (creat post))
 	if err := r.ParseForm(); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		WriteErrorLog("error.log", "failed to parse form: "+err.Error())
@@ -156,9 +136,6 @@ func handleCreatePost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if len(categories) == 0 {
-<<<<<<< HEAD
-		http.Redirect(w, r, "/?error=empty_categories#new-post", http.StatusSeeOther)
-=======
 		validationErrorList = append(validationErrorList, "categories_required")
 	}
 
@@ -171,7 +148,6 @@ func handleCreatePost(w http.ResponseWriter, r *http.Request) {
 			validationMutex.Unlock()
 		}
 		http.Redirect(w, r, "/#new-post", http.StatusSeeOther)
->>>>>>> dedef62 (Make a backend validation to max title lenght and body (creat post))
 		return
 	}
 
